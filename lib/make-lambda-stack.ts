bd35c2fda71e580b0f6cf2d63fb449c882f6f233
Clay Danford <crd013@gmail.com>
2024-05-01 21:16:02 -0500

    remove cdk

--- lib/make-lambda-stack.ts
+++ lib/make-lambda-stack.ts
@@ -2,14 +2,14 @@ import { Stack, StackProps, aws_lambda as lambda } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as path from 'path'
 
-export class CdkStack extends Stack {
+export class MakeLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
     const functionNames = ['createBook', 'getBooks']
 
     const layer = new lambda.LayerVersion(this, 'layer', {
-      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist/layer')),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../service/out/layer')),
       compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
       license: 'Apache-2.0',
     })
@@ -18,7 +18,7 @@ export class CdkStack extends Stack {
       new lambda.Function(this, fn, {
         runtime: lambda.Runtime.NODEJS_20_X,
         handler: `${fn}.handler`,
-        code: lambda.Code.fromAsset(path.join(__dirname, `../../service/out`), {
+        code: lambda.Code.fromAsset(path.join(__dirname, `../service/out`), {
           exclude: ['*', `!${fn}.js*`, '!package.json'],
         }),
         layers: [layer],
